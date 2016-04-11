7a21eef20a6c15f6528a3f9c1c689a20718914e2
Sadegh Dalvandi <dalvandi@gmail.com>
2016-04-11 19:15:18 +0100

    Update farsi_digits_to_latin.php

--- farsi_digits_to_latin.php
+++ farsi_digits_to_latin.php
@@ -1,9 +1,8 @@
+// Sadegh Dalvandi (www.dalvandi.com) - 25/03/2016 
+// Farsi/arabic digits in a string are converted to Latin digits so 
+// intval returns currect integer value.
+
 	function farsi_digits_to_latin($str){
-		
-		// Sadegh Dalvandi (www.dalvandi.com) - 25/03/2016 
-		// Farsi/arabic digits in a string are converted to Latin digits so 
-		// intval returns currect integer value.
-		
 		$conversion = array("dbb0"=>"0", "dbb1"=>"1", "dbb2"=>"2","dbb3"=>"3","dbb4"=>"4","dbb5"=>"5","dbb6"=>"6","dbb7"=>"7","dbb8"=>"8","dbb9"=>"9",
 		"d9a0"=>"0", "d9a1"=>"1", "d9a2"=>"2","d9a3"=>"3","d9a4"=>"4","d9a5"=>"5","d9a6"=>"6","d9a7"=>"7","d9a8"=>"8","d9a9"=>"9");
 		
